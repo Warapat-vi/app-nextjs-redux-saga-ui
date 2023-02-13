5624680b143857093255c9d2e9ce0d6a3c2763bc
Warapat Viboonsitthichok <warapat.vib@ascendcorp.com>
2023-02-13 10:10:31 +0700

    add user

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,10 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import _ from 'lodash'
-
+type user = {
+    name: string;
+    role: string;
+}
 type loginRespont = {
     token?: string;
-    message: string
+    user?: user;
+    message: string;
 }
 
 export default function login(
@@ -14,6 +18,9 @@ export default function login(
     if (_.isEmpty(req.body.email) || _.isEmpty(req.body.password)) {
         res.status(400).json({ message: 'Email and password can not be empty.' })
     } else {
-        res.status(200).json({ token: 'token suscess', message: 'suscess' })
+        res.status(200).json({ token: 'token suscess', message: 'suscess' ,user: {
+            name: 'Pro',
+            role: 'admin',
+        } })
     }
 }
